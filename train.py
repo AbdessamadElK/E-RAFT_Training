dafa6feeb5016b9183ac6402f8ad194e6ed29cdd
Abdessamad EL K. <abdessamad.elkaouri@gmail.com>
2023-10-28 13:33:51 +0100

    Fixing dstack bug by using torch.stack instead and squeezing the inputs

--- train.py
+++ train.py
@@ -253,11 +253,10 @@ def train(config):
                 print(flow.shape)
                 print(valid.shape)
                 # Visualize ground truth
-                gt_image, _ = visualize_optical_flow(torch.dstack([flow, valid]))
+                gt_image, _ = visualize_optical_flow(torch.stack(map(torch.squeeze, [flow, valid])))
                 writer.add_image("Ground truth", gt_image * 255)
-
                 # Visualize prediction
-                pred_image, _ = visualize_optical_flow(torch.dstack([flow_predictions[-1], valid]))
+                pred_image, _ = visualize_optical_flow(torch.stack(map(torch.squeeze, [flow_predictions[-1], valid])))
                 writer.add_image("Prediction", pred_image * 255)
 
     # logger.close()
