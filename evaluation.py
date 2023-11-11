d6849a1aedf8d14e9d4a999be555adfadf315360
Abdessamad EL K. <abdessamad.elkaouri@gmail.com>
2023-11-11 13:20:28 +0100

    squeezing event sequence

--- evaluation.py
+++ evaluation.py
@@ -53,7 +53,7 @@ def evaluate_dsec(model, val_loader, val_step, iters = 12, writer : SummaryWrite
             # Events as image
             event_sequence = data["raw_events_old"]
             print(event_sequence.shape)
-            event_img = events_to_event_image(event_sequence.numpy(), height, width)
+            event_img = events_to_event_image(event_sequence.numpy().squeeze(), height, width)
             event_img = event_img.numpy().transpose(1, 2, 0)
             top_row_content.append(event_img)
 
