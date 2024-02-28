d1cc3ef0a3e71e7fc4b94182d6e878e3389de8cb
Abdessamad EL K. <abdessamad.elkaouri@gmail.com>
2024-02-28 23:10:06 +0100

    Squeeze flow_gt and prediction

--- evaluation.py
+++ evaluation.py
@@ -55,22 +55,22 @@ def evaluate_dsec(model, dataset_provider, iters = 12, individual = False):
     for data in tqdm(data_loader, total=len(data_loader), desc="Evaluating", leave=False):
         volume_1 = data["event_volume_old"].cuda()
         volume_2 = data["event_volume_new"].cuda()
-        flow_gt = data["flow_gt"].cuda()
-        valid = data["flow_valid"].cuda()
+        flow_gt = data["flow_gt"].cuda().squeeze()
+        valid = data["flow_valid"].cuda().squeeze()
 
-        mag = torch.sum(flow_gt**2, dim=1).sqrt()
+        mag = torch.sum(flow_gt**2, dim=0).sqrt()
 
         valid = (valid >= 0.5) & (mag < MAX_FLOW)
 
         _, preds = model(volume_1, volume_2, iters)
-        prediction = preds[-1]
+        prediction = preds[-1].squeeze()
 
         if debug:
             print(prediction.shape)
             print(flow_gt.shape)
             debug = False
         
-        epe = torch.sum((prediction - flow_gt)**2, dim=1).sqrt()
+        epe = torch.sum((prediction - flow_gt)**2, dim=0).sqrt()
         epe_list.append(epe.cpu().view(-1).numpy())
 
         if individual:
