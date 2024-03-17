617e2428739282f6bc41a260cc671d82676f6f6b
Abdessamad EL K. <abdessamad.elkaouri@gmail.com>
2024-03-17 14:55:19 +0000

    Changed build_flow_dataset to also built the test split

--- build_flow_dataset.py
+++ build_flow_dataset.py
@@ -32,30 +32,35 @@ def build(in_path : Path, out_path : Path, policy = "copy"):
     """
     Build DSEC Flow dataset from the Downloads directory built after downloading the whole dataset.
     We assume that the downloads directory has the following structure:
-        Downloads
-        |- train_events
-            |- Sequence 1 (example : interlaken_00_a)
-                |- events
-                    |- left
-                    |- right
-            |- Sequence 2
+        DSEC_flow
+        |- train
+            |- train_events
+                |- Sequence 1 (example : interlaken_00_a)
+                    |- events
+                        |- left
+                        |- right
+                |- Sequence 2
+                |- ...
+            |- train_optical_flow
+                |- Sequence 1
+                    |- flow
+                        |- backward
+                        |- forward
+                        |- forward_timestamps.txt
+                        |- backward_timestamps.txt
+                |- Sequence 2
+                |- ...
+            |- train_images (optional)
+                |- Sequence 1
+                    |- images
+                        |- left
+                        |- right
+                        |- timestamps.txt
+                | - ...
+        |- test
+            |- test_events
             |- ...
-        |- train_optical_flow
-            |- Sequence 1
-                |- flow
-                    |- backward
-                    |- forward
-                    |- forward_timestamps.txt
-                    |- backward_timestamps.txt
-            |- Sequence 2
-            |- ...
-        |- train_images (optional)
-            |- Sequence 1
-                |- images
-                    |- left
-                    |- right
-                    |- timestamps.txt
-            | - ...
+
     
     The goal is to build DSEC_flow dataset which splits the data based on sequences and not the type of data,
     and only including sequences that have optical flow data available:
@@ -74,6 +79,9 @@ def build(in_path : Path, out_path : Path, policy = "copy"):
                 |- images_timestamps.txt
             |- zurich_city_02_a
             |- ...
+        |- test
+            |- interlaken_01_a
+            |- ...
     """
 
     assert in_path.is_dir()
@@ -81,52 +89,56 @@ def build(in_path : Path, out_path : Path, policy = "copy"):
     policy = policy.lower()
     assert policy in ["move", "copy"]
 
-    out_path = out_path / "train"
+    modes = [Path(uri).name for uri in in_path.iterdir()]
 
-    if not out_path.is_dir():
-        out_path.mkdir(parents = True, exist_ok = True)
+    for mode in modes:
+        mode_out_path = out_path / mode
 
-    flow_dir = in_path / "train_optical_flow"
-    
-    if flow_dir.is_dir():
-        flow_sequences = [x.name for x in flow_dir.iterdir()]
-    else:
-        flow_sequences = [x.name for x in out_path.iterdir()]
-    
-    for data_dir in in_path.iterdir():
+        if not mode_out_path.is_dir():
+            mode_out_path.mkdir(parents = True, exist_ok = True)
+
+        flow_dir = in_path / mode / f"{mode}_optical_flow"
         
-        oper = "Copying" if policy == "copy" else "Moving"
-        description = oper + " " + data_dir.name
-
-        for sequence_dir in tqdm(data_dir.iterdir(), desc=description):
-            if not sequence_dir.name in flow_sequences or sequence_dir.is_file():
-                continue
+        if flow_dir.is_dir():
+            flow_sequences = [x.name for x in flow_dir.iterdir()]
+        else:
+            flow_sequences = None
+        
+        for data_dir in (in_path/mode).iterdir():
             
-            overwrite = False
+            oper = "Copying" if policy == "copy" else "Moving"
+            description = oper + " " + data_dir.name
 
-            for sub_dir in sequence_dir.iterdir():
-                if sub_dir.is_file():
-                    continue
+            for sequence_dir in tqdm(data_dir.iterdir(), desc=description):
+                if flow_sequences is not None:
+                    if not sequence_dir.name in flow_sequences or sequence_dir.is_file():
+                        continue
+                
+                overwrite = False
 
-                for item in sub_dir.iterdir():
-                    name = "_".join([sub_dir.name, item.name])
-                    destination = out_path / sequence_dir.name / name
+                for sub_dir in sequence_dir.iterdir():
+                    if sub_dir.is_file():
+                        continue
 
-                    if (destination.is_dir() or destination.is_file()) and not overwrite:
-                        confirmation = input(str(data_dir) + " already exists, would you like to overwrite it? [y/N/a]")
-                        if confirmation.lower() in ["y", "yes"]:
-                            print("Warning :", destination.name, "will be overwriten. Conflicts might result from this.")
-                        elif confirmation.lower() in ["a", "all"]:
-                            overwrite = True
-                            print("Warning :", destination.name, "will be overwriten as well as every file and folder in this data directory. Conflicts might result from this.")
-                        else:
-                            print("Ignoring", destination.name)
-                            continue
+                    for item in sub_dir.iterdir():
+                        name = "_".join([sub_dir.name, item.name])
+                        destination = mode_out_path / sequence_dir.name / name
 
-                    copy_item(item, destination)                  
+                        if (destination.is_dir() or destination.is_file()) and not overwrite:
+                            confirmation = input(str(data_dir) + " already exists, would you like to overwrite it? [y/N/a]")
+                            if confirmation.lower() in ["y", "yes"]:
+                                print("Warning :", destination.name, "will be overwriten. Conflicts might result from this.")
+                            elif confirmation.lower() in ["a", "all"]:
+                                overwrite = True
+                                print("Warning :", destination.name, "will be overwriten as well as every file and folder in this data directory. Conflicts might result from this.")
+                            else:
+                                print("Ignoring", destination.name)
+                                continue
 
-                if policy == "move":
-                    shutil.rmtree(sub_dir)
+                        copy_item(item, destination)                  
+
+                    if policy == "move":
+                        shutil.rmtree(sub_dir)
     
     shutil.rmtree(in_path)
 
@@ -137,8 +149,7 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-i", "--input_path", type = str, help = "Downloaded dataset's directory path")
     parser.add_argument("-o", "--output_path", type = str, help= "DSEC-Flow output path")
-    parser.add_argument("-p", "--policy", type = str, default="copy", help= "Construction policy : Copy or Move")
-
+    parser.add_argument("-p", "--policy", type = str, default="copy", help= "Construction policy [[Copy] / Move]")
     args = parser.parse_args()
 
     input_path = Path(args.input_path)
